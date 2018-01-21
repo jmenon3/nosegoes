c9de629074997ac0b9a9561d5b597eff734e2918
rishitaro <riroy@ucsc.edu>
2018-01-21 10:52:02 -0800

    idk what I changed

--- public/houses.js
+++ public/houses.js
@@ -1,15 +1,10 @@
-<<<<<<< Updated upstream
 var firebaseref = firebase.database().ref();
 var user = firebase.auth().currentUser;
 firebase.auth().onAuthStateChanged(function(user) {
   if (user) {
-firebaseref.child("Users").child(user.uid).child("Houses").set("Homework");    
-    
+firebaseref.child("Users").child(user.uid).child("Houses").set("Homework");
+
   } else {
     alert("nahh");
   }
 });
-=======
-var shortid = require('shortid');
-console.log(shortid.generate());
->>>>>>> Stashed changes
--- public/index.js
+++ public/index.js
@@ -8,18 +8,17 @@ var firebaseref = firebase.database().ref();
 var user = firebase.auth().currentUser;
 
 firebase.auth().onAuthStateChanged(function(user) {
-<<<<<<< Updated upstream
   if (user) {
     firebaseref.child("Users").once('value', function(snapshot) {
   if (!snapshot.hasChild(user.uid)) {
     firebaseref.child("Users").child(user.uid).child("Email").set(user.email);
     firebaseref.child("Users").child(user.uid).child("Name").set(user.displayName);
-    firebaseref.child("Users").child(user.uid).child("Points").set("0"); 
-    
+    firebaseref.child("Users").child(user.uid).child("Points").set("0");
+
     window.location = 'houses.html'
-    
+
   }
-}); 
+});
   firebaseref.child("Users").child("Houses").once('value', function(snapshot) {
       //alert(!snapshot.hasChild("Houses"));
   if (!snapshot.hasChild("Houses")) {
@@ -29,26 +28,8 @@ firebase.auth().onAuthStateChanged(function(user) {
     window.location = 'index.html'
     break;
   }
-}); 
-    
-=======
-   if (user) {
-      firebaseref.child("Users").once('value', function(snapshot) {
-         if (!snapshot.hasChild(user.uid)) {
-            firebaseref.child("Users").child(user.uid).child("Email").set(user.email);
-            firebaseref.child("Users").child(user.uid).child("Name").set(user.displayName);
-            firebaseref.child("Users").child(user.uid).child("Houses").set("None");
-            firebaseref.child("Users").child(user.uid).child("Points").set("0");
-         }
-      });
+});
 
-      firebaseref.child("Users").child(user.uid).once('value', function(snapshot) {
-         if (!snapshot.hasChild("Associated-Houses")){
-            console.log("I'm in here!!");
-            // window.location = "houses.html";
-         }
-      });
->>>>>>> Stashed changes
 
   } else {
     // alert("nahh");
