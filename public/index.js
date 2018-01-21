284de9ffea3bc42926c320bf31010a99c6146600
rishitaro <riroy@ucsc.edu>
2018-01-21 01:24:04 -0800

    changes

--- public/index.js
+++ public/index.js
@@ -11,20 +11,12 @@ firebase.auth().onAuthStateChanged(function(user) {
   if (user) {
     firebaseref.child("Users").once('value', function(snapshot) {
   if (!snapshot.hasChild(user.uid)) {
-<<<<<<< HEAD
     firebaseref.child("Users").child(user.uid).child("Email").set(user.email);
     firebaseref.child("Users").child(user.uid).child("Name").set(user.displayName);
     firebaseref.child("Users").child(user.uid).child("Houses").set("None");
-    firebaseref.child("Users").child(user.uid).child("Points").set("1");
-=======
-    firebaseref.child("Users").child(user.uid).child("Email").set(user.email); 
-    firebaseref.child("Users").child(user.uid).child("Name").set(user.displayName);    
-    firebaseref.child("Users").child(user.uid).child("Houses").set("None");    
     firebaseref.child("Users").child(user.uid).child("Points").set("0");
->>>>>>> 1304e0f0e27ecd1994eb0d1d869ee0dbf9800dd9
   }
 });
-
 
   } else {
     // alert("nahh");
